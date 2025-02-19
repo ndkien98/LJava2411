a4cd3e98ce68e4a9635f5a2ab0b9b3de5fc9a798
kiennd25 <kiennd98@gmail.com>
2025-02-19 20:12:08 +0700

    init project

---
+++ LJava2411/spring-mvc/book-shop/book-shop/src/main/java/vn/com/t3h/dao/impl/BookRepositoryImpl.java
@@ -0,0 +1,6 @@
+package vn.com.t3h.dao.impl;
+
+import vn.com.t3h.dao.BookRepository;
+
+public class BookRepositoryImpl implements BookRepository {
+}
---
+++ LJava2411/spring-mvc/book-shop/book-shop/src/main/java/vn/com/t3h/service/impl/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package vn.com.t3h.service.impl;
+
+import org.springframework.stereotype.Service;
+import vn.com.t3h.dao.BookRepository;
+import vn.com.t3h.service.BookService;
+@Service
+public class BookServiceImpl implements BookService {
+
+
+}

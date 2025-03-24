637ac2cd2e234c12e80a5e8693830ab4ea4295c0
kiennd25 <kiennd98@gmail.com>
2025-03-24 21:44:52 +0700

    render table

--- LJava2411/spring-boot/source-code-claim-manager/src/main/resources/static/js/claim-manager/claim-manager.js
+++ LJava2411/spring-boot/source-code-claim-manager/src/main/resources/static/js/claim-manager/claim-manager.js
@@ -1,69 +1,17 @@
-var response = {
-    "code": 200,
-    "message": "Success",
-    "pageSize": 20,
-    "pageIndex": 0,
-    "totalPage": 1,
-    "totalElement": 7,
-    "data": [
-        {
-            "code": "C001",
-            "customerName": "John Doe",
-            "nameProduct": "Bảo hiểm xe hơi",
-            "claimDate": "2024-04-28",
-            "coverageProduct": "Bảo hiểm toàn diện cho xe hơi",
-            "statusName": "Khởi tạo"
-        },
-        {
-            "code": "C002",
-            "customerName": "Jane Smith",
-            "nameProduct": "Bảo hiểm sức khỏe",
-            "claimDate": "2023-08-12",
-            "coverageProduct": "Bảo hiểm sức khỏe toàn diện",
-            "statusName": "Nhân viên đã xác minh"
-        },
-        {
-            "code": "C003",
-            "customerName": "Bob Johnson",
-            "nameProduct": "Bảo hiểm nhà",
-            "claimDate": "2024-07-04",
-            "coverageProduct": "Bảo hiểm nhà toàn diện",
-            "statusName": "Chập nhận bồi thường"
-        },
-        {
-            "code": "C004",
-            "customerName": "John Doe",
-            "nameProduct": "Bảo hiểm du lịch",
-            "claimDate": "2024-03-30",
-            "coverageProduct": "Bảo hiểm du lịch toàn diện",
-            "statusName": "Nhân viên đang xử lý"
-        },
-        {
-            "code": "C005",
-            "customerName": "Jane Smith",
-            "nameProduct": "Bảo hiểm xe hơi",
-            "claimDate": "2024-03-23",
-            "coverageProduct": "Bảo hiểm toàn diện cho xe hơi",
-            "statusName": "Nhân viên đang xử lý"
-        },
-        {
-            "code": "C006",
-            "customerName": "John Doe",
-            "nameProduct": "Bảo hiểm sức khỏe",
-            "claimDate": "2024-03-30",
-            "coverageProduct": "Bảo hiểm sức khỏe toàn diện",
-            "statusName": "Thông tin chưa chính xác"
-        },
-        {
-            "code": "C007",
-            "customerName": "Jane Smith",
-            "nameProduct": "Bảo hiểm nhà",
-            "claimDate": "2024-03-23",
-            "coverageProduct": "Bảo hiểm nhà toàn diện",
-            "statusName": "Khởi tạo"
-        }
-    ]
-};
+function getAllClaimFromServer(){
+    fetch('http://localhost:8080/api/claim')
+        .then(response => {
+            if (!response.ok){
+                throw new Error('call api error');
+            }
+            // trả về json data
+            return response.json();
+        })
+        .then(dataResponse => {
+            console.log(dataResponse);
+            renderTable(dataResponse.data);
+        })
+}
 
 function renderTable(claims){
     // lay ra body cua table
@@ -89,4 +37,7 @@ function renderTable(claims){
     }
 }
 
-renderTable(response.data);
+// khi toàn bộ dữ liệu tại window đã load xong
+window.onload = function (){
+    getAllClaimFromServer();
+}
